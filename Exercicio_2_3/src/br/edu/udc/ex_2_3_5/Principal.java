7084260e0cd7eeb98f20804af5755c83a9561e0d
Helickson Foletto <helickson99@gmail.com>
2019-08-19 20:06:27 -0300

    Conclusão da parte 3.5

--- Exercicio_2_3/src/br/edu/udc/ex_2_3_5/Principal.java
+++ Exercicio_2_3/src/br/edu/udc/ex_2_3_5/Principal.java
@@ -1,25 +1,13 @@
 package br.edu.udc.ex_2_3_5;
 
-import java.util.Scanner;
-
 public class Principal {
 	
-	private static Scanner teclado;
-	
 	public static void main(String[] args) {
-		int numero;
 		
-		teclado = new Scanner (System.in);
-		
-		System.out.println("Entre com um número que tenha pelo menos 5 caracteres:");
-		numero = teclado.nextInt();
-		
-		Processamento leitura = new Processamento(numero);
-		
-		leitura.divisao(numero);
+		Processamento leitura = new Processamento();
+		leitura.entrada();
 		
 		System.out.println(leitura);
 
 	}
-
 }
--- Exercicio_2_3/src/br/edu/udc/ex_2_3_5/Processamento.java
+++ Exercicio_2_3/src/br/edu/udc/ex_2_3_5/Processamento.java
@@ -1,13 +1,14 @@
 package br.edu.udc.ex_2_3_5;
 
+import java.util.Scanner;
+
 public class Processamento {
 	int numero;
 	int primeiro, segundo, terceiro, quarto, quinto;
+	private Scanner teclado;
 	
-	public Processamento(int x) {
-		if(!verificaTamanho(x))
-			return;
-		this.numero = x;
+	public Processamento() {
+
 	}
 	public boolean verificaTamanho(int x) {
 		if((x >= 11111) && (x <= 99999))
@@ -28,6 +29,17 @@ public class Processamento {
 		segundo = numero % 10;
 		numero /= 10;
 		primeiro = numero % 10;
+	}
+	public void entrada() {
+		
+		teclado = new Scanner (System.in);
+		
+		System.out.println("Entre com um número que tenha pelo menos 5 caracteres:");
+		numero = teclado.nextInt();
+		
+		if(verificaTamanho(numero)) {
+			divisao(numero);
+		}		
 	}
 	@Override
 	public String toString() {
