320bf4208392e05756e664b89240c39663028bd4
Helickson Foletto <helickson99@gmail.com>
2019-08-26 20:30:13 -0300

    Inicio da questão 3 do capitulo 4

---
+++ Exercicio_4_3/src/br/edu/udc_4_3/Principal.java
@@ -0,0 +1,38 @@
+package br.edu.udc_4_3;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	private static Scanner leitura;
+
+	public static void main(String[] args) {
+
+		calculoMatematico(entrada());
+		
+	}
+	
+	public static int entrada() {
+		int x;
+		
+		leitura = new Scanner(System.in);
+		
+		System.out.println("Entre com o valor de X:");
+		x = leitura.nextInt();
+		
+		return x;
+	}
+	
+	public static void calculoMatematico(int x) {
+		System.out.println(x);
+		while(x >=0) {
+			if(x % 2 ==0) {
+				x = x /2;
+			}else {
+				x = ((2 * x) / 3) + 1;
+			}
+			System.out.println(x);
+						
+		}
+	}
+}
