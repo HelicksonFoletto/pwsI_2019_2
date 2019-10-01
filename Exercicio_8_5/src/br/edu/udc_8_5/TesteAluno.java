a59b7f01966b86e9bbf627181469a5f925f16a13
Helickson Foletto <helickson99@gmail.com>
2019-10-01 15:47:10 -0300

    Versão final do exercicio 5 do capitulo 8.

--- Exercicio_8_5/src/br/edu/udc_8_5/TesteAluno.java
+++ Exercicio_8_5/src/br/edu/udc_8_5/TesteAluno.java
@@ -7,19 +7,19 @@ public class TesteAluno {
 	private static Scanner sc;
 
 	public static void main(String[] args) {
-		Aluno al = new Aluno();
+		Aluno al1 = new Aluno();
 		sc = new Scanner(System.in);
 		
 		for(int i = 0; i < 4; i++) {
 			System.out.printf("Digite a %d° nota: ",i + 1);
-			al.notas[i] = sc.nextFloat();
+			al1.notas[i] = sc.nextFloat();
 			
 		}
 		System.out.println("Digite o nome do seu curso: ");
-		al.nomeDoCurso = sc.next();
+		al1.nomeDoCurso = sc.next();
 		
 		
-		if(al.aprovadoOuReprovado()) 
+		if(al1.aprovadoOuReprovado()) 
 			System.out.println("Você está aprovado!");
 		else
 			System.out.println("Você está reprovado!");
